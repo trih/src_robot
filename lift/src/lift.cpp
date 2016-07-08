5c939eafe6cc35637eeb5632c7914814fbada60e
Demo User <ubuntu@arm.localdomain>
2016-07-08 04:39:03 +0000

    fix lift.cpp file

--- lift/src/lift.cpp
+++ lift/src/lift.cpp
@@ -1,4 +1,5 @@
-nclude "BlackLib.h"
+#include "ros/ros.h"
+#include "BlackLib.h"
 #include <sstream>
 #include "std_msgs/String.h"
 #include <iostream>
@@ -9,13 +10,20 @@ using namespace std;
 
 void liftCallback(const std_msgs::String::ConstPtr& msg)
 {
+        BlackLib::BlackGPIO lift1(BlackLib::GPIO_115, BlackLib::output, BlackLib::SecureMode);
+        BlackLib::BlackGPIO lift2(BlackLib::GPIO_60, BlackLib::output, BlackLib::SecureMode);
     
         ROS_INFO("I heard: [%s]", msg->data.c_str());
         if (msg->data== "Up") {
+		lift1.setValue(BlackLib::low);
+		lift2.setValue(BlackLib::high);
 	}
 
         if (msg->data=="Down") {
-        }
+                lift1.setValue(BlackLib::high);
+                lift2.setValue(BlackLib::low);
+
+	}
 }
 
 int main(int argc, char **argv)
